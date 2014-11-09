c49a1ec141933e6caa04b9412f0732146c2b4067
saomocari <saomocari@gmail.com>
2014-11-09 20:32:41 +0900

    設定のコメント入れたりCSS微調整した

--- gallary.js
+++ gallary.js
@@ -1,18 +1,18 @@
 $(function(){
     
+    // 設定
     var options = {
-        thumbUl : $('#thumbnail'),
-        mainPhoto : $('#main_photo'),
-        parentDiv : $('#photo_container'),
-        slideSpeed: 3000,
-        fadeSpeed: 500,
-        startPlay: true,
-        maxWidth : 520, 
-        thumbMaxWidth : 80,  
-        thumbMinWidth : 65
+        thumbUl : $('#thumbnail'),  // サムネイルが入ったulのidを指定
+        mainPhoto : $('#main_photo'), // メイン画像が入るdivのidを指定
+        parentDiv : $('#photo_container'), // 全体を包むdivのidを指定
+        slideSpeed: 3000, // 次の画像に切り替わるまでの時間
+        fadeSpeed: 500, // フェードアニメーションの時間
+        startPlay: true, // 最初から自動再生しておくかどうか
+        maxWidth : 520, // parentDiv の max-width 値
+        thumbMaxWidth : 80,  // サムネイルの max-width 値
+        thumbMinWidth : 65 // サムネイルの min-width 値
     };
     
-    // 変数作る
     var thumbs = options.thumbUl.find('a'),
         mainPhoto = options.mainPhoto,
         thumbFiles = [],
@@ -30,7 +30,7 @@ $(function(){
         mainPhoto.height(mainPhoto.children('img').outerHeight());
     }
     
-    // 親Div サムネイルli メインDiv へ
+    // 親Div サムネイルli メインDiv へmax-width・width値などを設定する
     options.parentDiv.css('maxWidth', options.maxWidth);
     var liWidth = Math.floor((options.thumbMaxWidth / options.maxWidth) * 100);
     options.thumbUl.children('li').css({
@@ -41,28 +41,24 @@ $(function(){
 		
     // サムネイルとメイン画像の配列作る
     for(var i = 0; i < thumbs.length; i++){
-        // メインの配列
         mainFiles[i] = $('<img />');
         mainFiles[i].attr({
             src: $(thumbs[i]).attr('href'),
             alt: $(thumbs[i]).children('img').attr('alt')
         });
         mainFiles[i] = mainFiles[i][0];
-        // サムネイルの配列
         thumbFiles[i] = $(thumbs[i]).children('img');
         thumbFiles[i] = thumbFiles[i][0];
     }
-    
     // メインに最初の一枚を表示しておく
     mainPhoto.prepend(mainFiles[0]);
     // サムネイルの最初の一枚の親 li に current クラスを付ける
     $(thumbFiles[0]).parent().parent().addClass('current');
     
-	// ロード時の再生ボタンor停止ボタンの表示
+    // ロード時の再生ボタンor停止ボタンの表示
     if(options.startPlay) {
         autoPlay();
         playBtnHide();
-        playBtn.hide()
     } else {
         playBtnShow();
     }
@@ -71,13 +67,9 @@ $(function(){
     
     // サムネイルのクリックイベント
     thumbs.on('click', function(){
-        // クリックしたサムネイルが thumbFiles の何番目になるのかを調べる
         currentNum = $.inArray($("img",this)[0], thumbFiles); 
-        // 画像入れ替える関数実行
         mainView();
-        // 自動再生は止める
         stopPlay();
-        // 再生ボタンを表示
         playBtnShow();
         return false;
     });
@@ -88,11 +80,8 @@ $(function(){
         if(currentNum < 0){
 			currentNum = mainFiles.length - 1;
 		}
-        // 画像入れ替える関数実行
         mainView();
-        // 自動再生は止める
         stopPlay();
-        // 再生ボタンを表示
         playBtnShow();
     });
     
@@ -102,33 +91,26 @@ $(function(){
         if(currentNum > mainFiles.length - 1){
 			currentNum = 0;
 		}
-        // 画像入れ替える関数実行
         mainView();
-        // 自動再生は止める
         stopPlay();
-        // 再生ボタンを表示
         playBtnShow();
     });
     
     // 再生ボタンクリックイベント
     playBtn.on('click', function(){
 		if(nowPlay) return;
-        //自動再生オン
 		autoPlay();	
-		// ストップボタン表示
 		playBtnHide();
 	});
     
     // 停止ボタンクリックイベント
 	stopBtn.on('click', function(){
-		// 自動再生オフ
         stopPlay();
-        // プレイボタン表示
         playBtnShow();
         
 	});
     
-    // ウィンドウサイズ変更時 mainPhoto の高さも変更
+    // ウィンドウサイズ変更時 
     $(window).on('resize', function(){
         mainPhoto.height(mainPhoto.find('img').outerHeight());
     });
@@ -137,19 +119,16 @@ $(function(){
     
     // メイン画像入れ替える関数
     function mainView(){
-        // 画像を入れ替えて、古い方の画像はフェードアウトして削除
         mainPhoto.prepend(mainFiles[currentNum]).find('img').show();
         mainPhoto.find('img:not(:first)').stop(true, true).fadeOut(options.fadeSpeed, function(){
 		    $(this).remove();
 		});
         
-        // クリックしたサムネイルへ current クラスを付けかえる
         thumbs.eq(currentNum).parent().addClass('current').siblings().removeClass('current');
     }
 	
 	// 自動再生の関数
     function autoPlay(){
-        // タイマーで繰り返しメイン画像入れ替える処理をする
         nowPlay = true;
 		currentNum++;
         if(currentNum > mainFiles.length - 1){
